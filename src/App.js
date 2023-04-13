59e4ab1f3eda82ed1bcb29c17c77bec77e572bb6
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-13 17:06:36 +0200

    Small change to App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
     <div>
       <h1>Hello World</h1>
       <p>Hello, this is some text.</p>
-      <input type="text" />
-      <Button />
-      <Button />
-      <Button />
+      <input type="text" style={{ border: "1px solid black" }} />
+      <div style={{ display: "flex", gap: ".5rem" }}>
+        <Button />
+        <Button />
+        <Button />
+      </div>
     </div>
   );
 }
